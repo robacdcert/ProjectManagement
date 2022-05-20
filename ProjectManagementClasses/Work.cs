c1e4c20c74403c8e0fde707db585973465f9f9e5
Jared Burgoyne <jaredburgoyne3@gmail.com>
2022-05-20 10:51:05 -0400

    "Added AppDbContext"

--- ProjectManagementClasses/Work.cs
+++ ProjectManagementClasses/Work.cs
@@ -8,9 +8,14 @@ namespace ProjectManagementClasses
 {
     public class Work
     {
-        public static int Id { get; set; } = 0;
-        public static int ProjectId { get; set; } = 0;
+        public int Id { get; set; } = 0;
+        public int ProjectId { get; set; } = 0;
         public virtual Projects? Projects { get; set; }
+        public int ResourceId { get; set; }
+        public virtual Resources? Resources { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Hours { get; set; } = 0;
+
 
     }
 }
