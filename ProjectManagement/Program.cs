259e949575b532bcf62ccbaf7d1e2c5c622b9d87
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-20 15:11:00 -0400

    fixed work class

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -3,20 +3,4 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 
-const string server = "localhost\\sqlexpress";
-const string database = "ProjectManagementDb";
-
 var db = new AppDbContext();
-
-var workctrl = new WorkController(server, database);
-workctrl.OpenConnection();
-
-var projctrl = new ProjectsController(server, database);
-projctrl.OpenConnection();
-
-var resctrl = new ResourcesController(server, database);
-resctrl.OpenConnection();
-
-var work = workctrl.GetAllWorks();
-var projects = projctrl.GetAllProjects();
-var resources = resctrl.GetAllResources();
--- ProjectManagementClasses/AppDbContext.cs
+++ ProjectManagementClasses/AppDbContext.cs
@@ -15,15 +15,14 @@ namespace ProjectManagementClasses
         public virtual DbSet<Resources> Resources { get; set; } = null!;
 
         public AppDbContext() { }
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
-
-            var connStr = @"server=localhost\sqlexpress;database=ProjectManagementDb;trustServerCertificate=true;trusted_connection=true";
+            var connStr = @"server=localhost\sqlexpress;
+                            database=ProjectManagementDb;
+                            trustServerCertificate=true;
+                            trusted_connection=true";
 
             builder.UseSqlServer(connStr);
         }
