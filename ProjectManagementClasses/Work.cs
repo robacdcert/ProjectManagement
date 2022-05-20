837671ab8959b4e40b268c757f85e508c874cb9e
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-20 14:50:36 -0400

    fixed?

--- ProjectManagementClasses/Work.cs
+++ ProjectManagementClasses/Work.cs
@@ -9,10 +9,10 @@ namespace ProjectManagementClasses
     public class Work
     {
         public int Id { get; set; } = 0;
-        public int ProjectsId { get; set; } = 0;
-        public virtual Projects? Projects { get; set; }
-        public int ResourcesId { get; set; }
-        public virtual Resources? Resources { get; set; }
+        public int? ProjectsId { get; set; } = 0;
+        public virtual Projects? Projects { get; set; } = null;
+        public int? ResourcesId { get; set; } = 0;
+        public virtual Resources? Resources { get; set; } = null;
         public string Description { get; set; } = string.Empty;
         public int Hours { get; set; } = 0;
 
