61af5338b131d5977cfaf3fdeacac92c5a2cef07
robacdcert <Robert.Griffin@IVP-LD6V08MG3>
2022-05-20 10:41:05 -0400

    Created beginning of program file

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using ProjectManagementClasses;
+
+Console.WriteLine("Welcome to Project Management! Select from the following:");
+Console.WriteLine("'1'   Allocate resources to a project");
+Console.WriteLine("'2'   Record work by resource");
+Console.WriteLine("'3'   Update projext");
+var input = Console.ReadLine();
+if (input == "1")
+{
+
+}
