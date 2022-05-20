784c60a473bac9df3376e0a02a915b4d77b43370
Jared Burgoyne <jaredburgoyne3@gmail.com>
2022-05-20 09:47:00 -0400

    Added Properties To Classes

--- ProjectManagementClasses/Projects.cs
+++ ProjectManagementClasses/Projects.cs
@@ -2,6 +2,11 @@
 {
     public class Projects
     {
+        public static int Id { get; set; } = 0;
+        public static string Description { get; set; } = string.Empty;
+        public static int EstimatedHours { get; set; } = 0;
+        public static int ActualHours { get; set; } = 0;
+        public static string Status { get; set; } = "New";
 
     }
 }
